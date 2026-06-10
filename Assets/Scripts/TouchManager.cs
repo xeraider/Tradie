a072a348c158d07a47cea005b9b73f726dd87d08
xeraider <janithaenuka@gmail.com>
2026-06-10 23:22:25 +1200

    Touchscreen
    - Added a touch screen manager object
    - Added a script for touch manager to handle a press and the position of the press.

---
+++ Assets/Scripts/TouchManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// Handles touch input.
+/// </summary>
+public class TouchManager : MonoBehaviour
+{
+    private Camera mainCamera;
+    private PlayerInput playerInput;
+
+    private InputAction touchPressAction;
+    private InputAction touchPositionAction;
+
+    private void Awake()
+    {
+        
+        mainCamera = Camera.main;
+        playerInput = GetComponent<PlayerInput>();
+
+        // Get input actions from Input System
+        touchPressAction = playerInput.actions.FindAction("TouchPress");
+        touchPositionAction = playerInput.actions.FindAction("TouchPosition");
+
+        Debug.Assert(touchPressAction != null, "TouchPress action not found.");
+        Debug.Assert(touchPositionAction != null, "TouchPosition action not found.");
+    }
+
+    private void OnEnable()
+    {
+        touchPressAction.performed += TouchPressed;
+    }
+
+    private void OnDisable()
+    {
+        touchPressAction.performed -= TouchPressed;
+    }
+
+    /// <summary>
+    /// Called when the screen is tapped.
+    /// </summary>
+    private void TouchPressed(InputAction.CallbackContext context)
+    {
+        // Convert touch position to world position
+        Vector2 worldPos = mainCamera.ScreenToWorldPoint(
+            touchPositionAction.ReadValue<Vector2>());
+    }
+}
